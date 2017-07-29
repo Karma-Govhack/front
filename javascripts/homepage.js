d02ba988c7466e99a33061b5fe9bf84388568b8a
emi <emi.mnemonic@gmail.com>
2017-07-30 10:38:43 +1200

    Add toggle registration forms

---
+++ javascripts/homepage.js
@@ -0,0 +1,9 @@
+function toggleVolunteerForm() {
+  $('#homepageOptions').addClass("hidden");
+  $('#volunteerRegForm').removeClass("hidden");
+}
+
+function toggleCharityForm() {
+  $('#homepageOptions').addClass("hidden");
+  $('#charityRegForm').removeClass("hidden");
+}
